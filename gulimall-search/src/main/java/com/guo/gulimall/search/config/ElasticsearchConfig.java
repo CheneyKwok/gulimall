990a3e225d8df25d16adac16fd200b5ef999390f
郭志成 <2399214024@qq.com>
2021-07-24 18:55:14 +0800

    云服务配置

--- gulimall-search/src/main/java/com/guo/gulimall/search/config/ElasticsearchConfig.java
+++ gulimall-search/src/main/java/com/guo/gulimall/search/config/ElasticsearchConfig.java
@@ -28,6 +28,6 @@ public class ElasticsearchConfig {
     public RestHighLevelClient esRestClient() {
         return new RestHighLevelClient(
                 RestClient.builder(
-                        new HttpHost("192.168.56.10", 9200, "http")));
+                        new HttpHost("115.29.195.125", 9200, "http")));
     }
 }
