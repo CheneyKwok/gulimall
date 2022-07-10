72fb4b8bc82809fad8fac310800dd16f1a4b522c
CheneyKwok <68406855+CheneyKwok@users.noreply.github.com>
2022-07-10 14:21:10 +0800

    RabbitMQ：消费端确认

--- gulimall-order/src/main/java/com/guo/gulimall/order/service/RabbitMQTestService.java
+++ gulimall-order/src/main/java/com/guo/gulimall/order/service/RabbitMQTestService.java
@@ -10,13 +10,15 @@ import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+
 
 @Slf4j
 @Service
 @RabbitListener(queues = {"hello-java-queue"})
 public class RabbitMQTestService {
 
-    @RabbitHandler
+//    @RabbitHandler
     public void receiveMessage(Message message, OrderReturnReasonEntity entity, Channel channel) {
         log.info("收到消息....{}", entity);
     }
@@ -24,6 +26,27 @@ public class RabbitMQTestService {
     @RabbitHandler
     public void receiveMessage(Message message, OrderEntity entity, Channel channel) {
         log.info("收到消息....{}", entity);
+    }
+
+
+    @RabbitHandler
+    public void receiveMessageByManualAck(Message message, OrderReturnReasonEntity entity, Channel channel) {
+        // 2、4、6、8、10
+        long deliveryTag = message.getMessageProperties().getDeliveryTag();
+        log.info("收到消息... DeliveryTag = {}", deliveryTag);
+        try {
+            if (deliveryTag == 4) {
+                channel.basicAck(deliveryTag, true);
+            } else if (deliveryTag == 6) {
+                channel.basicAck(deliveryTag, false);
+            } else if (deliveryTag == 8) {
+                channel.basicNack(deliveryTag, false, true);
+            } else if (deliveryTag == 10) {
+                channel.basicReject(deliveryTag, false);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
